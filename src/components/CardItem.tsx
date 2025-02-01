9dc3ea52e191671aee4b595dba8e742d0e5ba680
LukaDudauri <lukadudauri01@gmail.com>
2025-02-01 16:46:05 +0400

    carditem/cardbody

---
+++ src/components/CardItem.tsx
@@ -0,0 +1,9 @@
+import { CardItemProps } from "../types/types";
+
+function CardItem({component: Component = "div", children, className, ...rest}: CardItemProps) {
+    return <Component className={className} {...rest}>
+        {children}
+    </Component>
+}
+
+export default CardItem;
--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,15 @@ export interface CardContainerProps {
     children?: React.ReactNode;
     className?: string;
 }
+
+export interface CardBodyProps {
+    children?: React.ReactNode;
+    className?: string;
+}
+
+export interface CardItemProps {
+    component?: React.ElementType;
+    children?: React.ReactNode;
+    className?: string;
+    [key: string]: unknown;
+}
