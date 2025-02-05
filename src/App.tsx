d50c9c4325d19354b7b824a1f384ab8c3bb12403
LukaDudauri <lukadudauri01@gmail.com>
2025-02-05 13:34:42 +0400

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import CardContainer from "./components/CardContainer";
+import CardBody from "./components/CardBody";
+import CardItem from "./components/CardItem";
+
+import product from "./data/data.json"
+
 function App() {
-  return <div>Hello</div>
+  return <div className="h-screen bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-[#ba78bd] to-[#613177] ">
+    <CardContainer className="inet-var">
+      <CardBody className="group/card flex flex-col gap-5 rounded-x1 bg-[radial-gradient(circle_at_center,_var
+      (--tw-gradient-stops))] from-[#ba78bd] to-[#613177] p-6 hover:shadow-lg">
+        <CardItem className="mt-4 w-full">
+          <img src={product.product.src} alt={product.product.title} />
+        </CardItem>
+      </CardBody>
+    </CardContainer>
+  </div>
 }
 
 export default App;
--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -1,9 +1,10 @@
 import { CardContainerProps } from "../types/types";
+import { cn } from "../utils/cn";
 
 function CardContainer({ children, className }: CardContainerProps){
 
     return <div className="flex items-center justify-between py-20">
-        <div className="relative flex items-center">
+        <div className={cn("relative flex items-center", className)}>
             {children}
         </div>
     </div>
