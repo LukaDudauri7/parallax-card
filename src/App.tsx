3e601cbd72358235c742ad9e2e9361337404926f
LukaDudauri <lukadudauri01@gmail.com>
2025-02-05 13:38:01 +0400

    .

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,18 @@ import product from "./data/data.json"
 function App() {
   return <div className="h-screen bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-[#ba78bd] to-[#613177] ">
     <CardContainer className="inet-var">
-      <CardBody className="group/card flex flex-col gap-5 rounded-x1 bg-[radial-gradient(circle_at_center,_var
+      <CardBody className="group/card flex flex-col gap-5 rounded-xl bg-[radial-gradient(circle_at_center,_var
       (--tw-gradient-stops))] from-[#ba78bd] to-[#613177] p-6 hover:shadow-lg">
         <CardItem className="mt-4 w-full">
           <img src={product.product.src} alt={product.product.title} />
+        </CardItem>
+
+        <CardItem className="text-5xl font-bold text-gray-50">
+          {product.product.title}
+        </CardItem>
+
+        <CardItem className="mt-2 max-w-sm text-xl font-semibold text-[#ffbd78]">
+          {product.product.description}
         </CardItem>
       </CardBody>
     </CardContainer>
