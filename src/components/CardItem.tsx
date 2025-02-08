daeac5fa97fc0a73bebfde8855c3ad6b36f0b62c
LukaDudauri <lukadudauri01@gmail.com>
2025-02-08 16:51:47 +0400

    ..

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,9 +1,16 @@
 import { CardItemProps } from "../types/types";
 
-function CardItem({component: Component = "div", children, className, ...rest}: CardItemProps) {
-    return <Component className={className} {...rest}>
-        {children}
-    </Component>
+function CardItem({
+    component: Component = "div",
+    children, 
+    className, 
+    ...rest
+}: CardItemProps) {
+    return (
+        <Component className={className} {...rest}>
+            {children}
+        </Component>
+    )
 }
 
 export default CardItem;
